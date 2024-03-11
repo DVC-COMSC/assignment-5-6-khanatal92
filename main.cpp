361760d376ad0f7a0648b0c99dee4f7d8851e9e0
khanatal92 <akhan126@insite.4cd.edu>
2024-03-11 12:40:15 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main()
 	int N;
 	cin >> N;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = i; j < N; j++) {
+	for (int i = 0; i < N; ++i) {
+		for (int j = i; j < N; ++j) {
 			cout << i << ", " << j << endl;
 		}
 	}
