ca3ad3f6c3e6071bf4096115695b44efe5c3203f
khanatal92 <akhan126@insite.4cd.edu>
2024-03-11 12:51:15 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -4,16 +4,15 @@ using namespace std;
 
 int main()
 {
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
 
-	for (int i = 0; i < N; ++i) {
-		for (int j = i; j < N; ++j) {
-			if ((i+j)>(N-2))
-			{
-			cout << i << ", " << j << endl;
-		}
-	}
-
-	return 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = N - 1; j >= 0; j--) {
+			if (j >= i) {
+            cout << i << ", " << j << endl;
+        }
+   	 }
+}
+    return 0;
 }
